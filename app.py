b72583028dcac43ddb0cca9a422aa68fd17b7e8c
lilbrolilbrohaha <goofbastard@gmail.com>
2026-01-28 16:01:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 import requests
-from flask import Flask, request
+import os
+from flask import Flask, request, send_file
 
 
 app = Flask(__name__)
 
 
-@app.before_request
-def before_request():
+@app.route("/<path:name>")
+def route(name):
+    try:
+        if os.path.exists(f"cache/{name}"):
+            return send_file(f"cache/{name}")
+    except:
+        pass
+
     response = requests.request(
         url="http://216.9.225.189:9999" + request.path,
         method=request.method,
         data=request.get_data(),
         headers=request.headers
     )
+    if response.ok and os.path.splitext(name)[1]:
+         dir = os.path.dirname(f"cache/{name}")
+         os.makedirs(dir, exist_ok=True)
+         with open(f"cache/{name}", "wb") as f:
+             f.write(response.content)
     return response.content, response.status_code, {"Content-Type": response.headers.get("Content-Type")}
 
 
